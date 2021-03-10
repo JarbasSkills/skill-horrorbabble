a92b6edfb571492ca59beb7883c4cdb26c1a5ad3
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-03-10 10:09:20 -1000

    bettercps (#1)
    Co-authored-by: jarbasal <jarbasai@mailfence.com>

--- __init__.py
+++ __init__.py
@@ -1,26 +1,29 @@
-from ovos_utils.waiting_for_mycroft.common_play import CPSMatchType, CPSMatchLevel
-from ovos_utils.skills.templates.media_collection import MediaCollectionSkill
+from pyvod import Collection
+from os.path import join, dirname, basename
+from ovos_utils.skills.templates.video_collection import VideoCollectionSkill
 from mycroft.skills.core import intent_file_handler
-from mycroft.util.parse import fuzzy_match, match_one
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-import random
-from json_database import JsonStorageXDG
-import datetime
+from ovos_utils.playback import CPSMatchType, CPSPlayback, CPSMatchConfidence
 
 
-class HorrorBabbleSkill(MediaCollectionSkill):
+class HorrorBabbleSkill(VideoCollectionSkill):
 
     def __init__(self):
         super().__init__("HorrorBabble")
         self.supported_media = [CPSMatchType.GENERIC,
                                 CPSMatchType.AUDIOBOOK,
                                 CPSMatchType.VIDEO]
-
+        self.default_image = join(dirname(__file__), "ui", "bg.png")
+        self.skill_logo = join(dirname(__file__), "ui", "horrorbabble_icon.png")
+        self.skill_icon = join(dirname(__file__), "ui", "horrorbabble_icon.png")
+        self.default_bg = join(dirname(__file__), "ui", "bg.png")
         path = join(dirname(__file__), "res", "HorrorBabble.jsondb")
         logo = join(dirname(__file__), "res",  "horrorbabble_logo.png")
         # load video catalog
         self.media_collection = Collection("HorrorBabble", logo=logo, db_path=path)
+        self.media_type = CPSMatchType.AUDIOBOOK
+        self.playback_type = CPSPlayback.AUDIO
 
     def get_intro_message(self):
         self.speak_dialog("intro")
@@ -31,34 +34,21 @@ class HorrorBabbleSkill(MediaCollectionSkill):
 
     # matching
     def match_media_type(self, phrase, media_type):
-        match = None
         score = 0
-
-        if self.voc_match(phrase,
-                          "video") or media_type == CPSMatchType.VIDEO:
-            score += 0.05
-            match = CPSMatchLevel.GENERIC
 
         if self.voc_match(phrase, "audiobook") or\
                 media_type == CPSMatchType.AUDIOBOOK:
-            score += 0.1
-            match = CPSMatchLevel.CATEGORY
+            score += 10
+            if self.voc_match(phrase, "horror"):
+                score += 30
+                self.CPS_extend_timeout(1)
 
-        if self.voc_match(phrase,
-                          "horrorstory"):
-            score += 0.1
-            match = CPSMatchLevel.CATEGORY
+        if self.voc_match(phrase, "horrorstory"):
+            score += 10
 
         if self.voc_match(phrase, "horrorbabble"):
-            score += 0.3
-            match = CPSMatchLevel.TITLE
+            score += 80
 
-        return match, score
-
-    def calc_final_score(self, phrase, base_score, match_level):
-        score = base_score
-        if self.voc_match(phrase, "horrorbabble"):
-            score += 0.15
         return score
 
 
