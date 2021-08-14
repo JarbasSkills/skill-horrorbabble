4abdd4ddaad94df7cc2e56c3273eb2350834e8a0
jarbasal <jarbasai@mailfence.com>
2021-08-14 14:02:15 +0100

    .

--- __init__.py
+++ __init__.py
@@ -3,28 +3,30 @@ from os.path import join, dirname, basename
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
+    CommonPlayMatchConfidence
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
+import biblioteca
 
 
 class HorrorBabbleSkill(VideoCollectionSkill):
 
     def __init__(self):
         super().__init__("HorrorBabble")
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.AUDIOBOOK,
-                                CPSMatchType.VIDEO]
+        self.supported_media = [CommonPlayMediaType.GENERIC,
+                                CommonPlayMediaType.AUDIOBOOK,
+                                CommonPlayMediaType.VIDEO]
         self.default_image = join(dirname(__file__), "ui", "bg.png")
         self.skill_logo = join(dirname(__file__), "ui", "horrorbabble_icon.png")
         self.skill_icon = join(dirname(__file__), "ui", "horrorbabble_icon.png")
         self.default_bg = join(dirname(__file__), "ui", "bg.png")
-        path = join(dirname(__file__), "res", "HorrorBabble.jsondb")
+        base_folder = biblioteca.download("ytcat_horrorbabble")
+        path = join(base_folder, "horrorbabble.jsondb")
         logo = join(dirname(__file__), "res",  "horrorbabble_logo.png")
         # load video catalog
-        self.media_collection = Collection("HorrorBabble", logo=logo, db_path=path)
-        self.media_type = CPSMatchType.AUDIOBOOK
-        self.playback_type = CPSPlayback.AUDIO
+        self.media_collection = Collection("horrorbabble", logo=logo, db_path=path)
+        self.media_type = CommonPlayMediaType.AUDIOBOOK
+        self.playback_type = CommonPlayPlaybackType.AUDIO
 
     def get_intro_message(self):
         self.speak_dialog("intro")
@@ -38,7 +40,7 @@ class HorrorBabbleSkill(VideoCollectionSkill):
         score = 0
 
         if self.voc_match(phrase, "audiobook") or\
-                media_type == CPSMatchType.AUDIOBOOK:
+                media_type == CommonPlayMediaType.AUDIOBOOK:
             score += 10
             if self.voc_match(phrase, "horror"):
                 score += 30
